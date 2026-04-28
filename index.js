7a0e55719174be17ba3f90ba6d1e7b8271918c69
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-28 20:48:00 +0600

    create index.js

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const express = require('express')
-const cors = require('cors')
-const port = process.env.PORT || 5000;
-const app = express()
-const client = require('./src/helpers/client');
-require('dotenv').config()
+// const express = require('express')
+// const cors = require('cors')
+// const port = process.env.PORT || 5000;
+// const app = express()
+// const client = require('./src/helpers/client');
+// require('dotenv').config()
 
 
-// ----------- Import Routes -----------
-const propertiesRoutes = require('./src/routes/PropertiesRoutes');
-const authRoutes = require('./src/routes/authRoutes');
+// // ----------- Import Routes -----------
+// const propertiesRoutes = require('./src/routes/PropertiesRoutes');
+// const authRoutes = require('./src/routes/authRoutes');
 
 
 
-// ----------- MIDDLEWARES -----------
-app.use(cors());
-app.use(express.json());
+// // ----------- MIDDLEWARES -----------
+// app.use(cors());
+// app.use(express.json());
 
-async function run() {
-  try {
-    await client.connect();
-    console.log("MongoDB connected successfully !");
-
-
-    // ----------- Routes -----------
-    app.use('/properties', propertiesRoutes);
-    app.use('/auth', authRoutes);
+// async function run() {
+//   try {
+//     await client.connect();
+//     console.log("MongoDB connected successfully !");
 
 
-    // ----------- Root --------------
-    app.get('/', async(req, res) => {
-      await client.db("admin").command({ ping: 1 })
-      res.send('users managment server is running')
-    })
+//     // ----------- Routes -----------
+//     app.use('/properties', propertiesRoutes);
+//     app.use('/auth', authRoutes);
 
 
-    // ----------- 404 -----------
-    app.use((req, res) => {
-      res.status(404).json({ success: false, message: "Route not found" });
-    });
-
-    // ----------- SERVER RUN -----------
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+//     // ----------- Root --------------
+//     app.get('/', async(req, res) => {
+//       await client.db("admin").command({ ping: 1 })
+//       res.send('users managment server is running')
+//     })
 
 
-  }
-  catch (error) {
-    console.error("Error connecting to DB:", error);
-  }
-}
-run().catch(console.dir);
+//     // ----------- 404 -----------
+//     app.use((req, res) => {
+//       res.status(404).json({ success: false, message: "Route not found" });
+//     });
+
+//     // ----------- SERVER RUN -----------
+//     app.listen(port, () => {
+//       console.log(`Example app listening on port ${port}`)
+//     })
+
+
+//   }
+//   catch (error) {
+//     console.error("Error connecting to DB:", error);
+//   }
+// }
+// run().catch(console.dir);
 
