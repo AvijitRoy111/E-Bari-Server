8643bc64ae5bc2ce1dfcb84d1b46f4bfe4e3cf84
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-28 21:00:04 +0600

    added the resul

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ const userCollection = client.db('E-Bari').collection('Users');
 // create user............
 const createUser = async (req , res ) =>{
     const user = req.body;
-//     const result = await userCollection.insertOne(user);
+    const result = await userCollection.insertOne(user);
 //     res.status(200).json({success:true, message:"user created successfully", data:result});
 // }
 
