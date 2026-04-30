0a1cd1591fe653b1d13a279bd64625fa6be8d206
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-30 15:23:56 +0600

    added the router update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
-const {objectId} = require ('mongodb');
-const client = require('../helpers/client');
+// const {objectId} = require ('mongodb');
+// const client = require('../helpers/client');
 
-const userCollection = client.db('E-Bari').collection('Users');
+// const userCollection = client.db('E-Bari').collection('Users');
 
-// create user............
-const createUser = async (req , res ) =>{
-    const user = req.body;
-    const result = await userCollection.insertOne(user);
-    res.status(200).json({success:true, message:"user created successfully", data:result});
-}
+// // create user............
+// const createUser = async (req , res ) =>{
+//     const user = req.body;
+//     const result = await userCollection.insertOne(user);
+//     res.status(200).json({success:true, message:"user created successfully", data:result});
+// }
 
-module.exports = {
-    createUser,
+// module.exports = {
+//     createUser,
     
-}
+// }
