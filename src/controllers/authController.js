70363b4016ac53bf5e8645c52469a7ba707d6ec6
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-04-30 15:24:26 +0600

    added the helper

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const {objectId} = require ('mongodb');
-// const client = require('../helpers/client');
+const client = require('../helpers/client');
 
 // const userCollection = client.db('E-Bari').collection('Users');
 
