1ada7e0aa5297e843175b45f6b6ba5ae7246bd00
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-01 13:57:52 +0600

    added the module

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,7 +10,7 @@ const createUser = async (req , res ) =>{
     res.status(200).json({success:true, message:"user created successfully", data:result});
 }
 
-// module.exports = {
+module.exports = {
 //     createUser,
     
 // }
