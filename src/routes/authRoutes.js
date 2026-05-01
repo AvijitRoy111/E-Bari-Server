ed4bef62105f385ada29a78a5310961ee6ab945b
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-01 14:00:34 +0600

    added the i,poprt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
     createUser,
 
-// } = require('../controllers/authController')
+} = require('../controllers/authController')
 
 
 // const router = express.Router();
