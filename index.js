da7a6ce3343c2ca9ce19423e7abd52d171dda151
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-02 08:10:24 +0600

    added the app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 const app = express()
-// const client = require('./src/helpers/client');
+const client = require('./src/helpers/client');
 // require('dotenv').config()
 
 
