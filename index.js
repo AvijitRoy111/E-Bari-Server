2e5642da58566d21a2199b71f5c020f370059315
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-02 08:11:08 +0600

    added the dot

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 const app = express()
 const client = require('./src/helpers/client');
-// require('dotenv').config()
+require('dotenv').config()
 
 
 // ----------- Import Routes -----------
