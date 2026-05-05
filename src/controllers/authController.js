974bba0e02247c4d94d2ee2bdbcaddd50e1d2a68
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-05 16:03:13 +0600

    added the connec

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,11 @@
 const { ObjectId } = require('mongodb');
 const client = require('../helpers/client');
 
-// const userCollection = client.db('E-Bari').collection('Users');
+const userCollection = client.db('E-Bari').collection('Users');
 
-// // create user............
-// const createUser = async (req, res) => {
-//     try {
+// create user............
+const createUser = async (req, res) => {
+    try {
 //         const user = req.body;
 //         // ১. check if user already exists
 //         const existingUser = await userCollection.findOne({ email: user.email });
