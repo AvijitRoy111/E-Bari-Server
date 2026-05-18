0b74efb5da568efcaebec8f14cc84976f9df0981
AvijitRoy111 <bormonbidhan12@example.com>
2026-05-18 13:22:25 -0700

    added the booking

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -3,8 +3,8 @@ const client = require('../helpers/client');
 
 const bookingCollection = client.db('E-Bari').collection('bookings');
 
-// // create booking .............
-// const createBooking = async (req, res) =>{
+// create booking .............
+const createBooking = async (req, res) =>{
 //     const booking = req.body;
 //     const result = await bookingCollection.insertOne(booking);
 //     res.status(200).json({success:true, message:"booking created successfully", data:result});
