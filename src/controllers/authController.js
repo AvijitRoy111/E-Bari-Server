483b4ead2c7f69d2adf6c32b76a7ac13ab82c4d2
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-20 00:21:04 +0600

    added the get user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -28,8 +28,8 @@ const createUser = async (req, res) => {
     }
 }
 
-// // get current user ........
-// const getCurrentUser = async (req, res) =>{
+ // get current user ........
+ const getCurrentUser = async (req, res) =>{
 //     const email =req.query.email;
 //     const user = await userCollection.findOne({email:email});
 //     if(!user){
