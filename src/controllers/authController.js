4f2662ee68c4bd634ced457518b0c4e74bed5e8a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-20 00:25:01 +0600

    added the await

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -50,7 +50,7 @@ const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-//         // ১. user find
+        // ১. user find
 //         const user = await userCollection.findOne({ email: email });
         
 //         if (!user) {
