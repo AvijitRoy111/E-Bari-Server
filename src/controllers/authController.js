6ef0b21c50ece1132eae0a3ebed4e54b886a93fa
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-05-21 12:48:27 +0600

    added the catch

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -77,7 +77,7 @@ const loginUser = async (req, res) => {
          });
 
      } catch (error) {
-//         res.status(500).json({ success: false, message: "Internal Server Error" });
+         res.status(500).json({ success: false, message: "Internal Server Error" });
 //     }
 // }
 
