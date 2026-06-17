45d64600d0079b5d84ebdd2211c3576f5f7fb387
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-17 12:19:11 +0600

    added the contact

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -5,7 +5,7 @@ const contactCollection = client.db('E-Bari').collection('contacts');
 
 // create contact .............
 const createContact = async (req, res) =>{
-//     const contact = req.body;
+    const contact = req.body;
 //     const result = await contactCollection.insertOne(contact);
 //     res.status(200).json({success:true, message:"contact created successfully", data:result});
 
