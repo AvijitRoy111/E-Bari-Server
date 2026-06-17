a784257a27914ebe6772b88f44450eb5de6695a6
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-17 12:15:51 +0600

    added the client

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -3,7 +3,7 @@ const client = require('../helpers/client');
 
 const bookingCollection = client.db('E-Bari').collection('bookings');
 
-// // create booking .............
+// create booking .............
 // const createBooking = async (req, res) =>{
 //     const booking = req.body;
 //     const result = await bookingCollection.insertOne(booking);
