3b402a6f4a714578b7b1a11975a753f650e011d5
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-18 12:39:17 +0600

    added the client

--- src/controllers/PropertiesControllers.js
+++ src/controllers/PropertiesControllers.js
@@ -1,4 +1,4 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 // const client = require('../helpers/client');
 
 // const propertiesCollection = client.db('E-Bari').collection('properties');
