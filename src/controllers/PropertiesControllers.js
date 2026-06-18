26448b213101cf00886c831b72ac24a93447aaad
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-18 12:39:46 +0600

    added the collection

--- src/controllers/PropertiesControllers.js
+++ src/controllers/PropertiesControllers.js
@@ -4,7 +4,7 @@ const client = require('../helpers/client');
 const propertiesCollection = client.db('E-Bari').collection('properties');
 
 
-// // get all properties............
+// get all properties............
 // const getProperties = async (req, res) =>{
 //     const properties = await propertiesCollection.find().toArray();
 //     res.status(200).json({success:true, message:"all properties", data:properties});
