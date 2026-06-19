0395a90fafed9ddeebfc29abe8d8b9774979b41c
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-19 12:07:09 +0600

    added the status

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -26,7 +26,7 @@ const createUser = async (req, res) => {
      } catch (error) {
          res.status(500).json({ success: false, message: "Internal Server Error" });
      }
-// }
+ }
 
 //  // get current user ........
 //  const getCurrentUser = async (req, res) =>{
