e67541b10f9ef10dc57ac47ca2e7a801ff9dc75a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-19 12:04:53 +0600

    added the create user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ const createUser = async (req, res) => {
         }
          // ২.create new user
          const result = await userCollection.insertOne(user);
-//         res.status(200).json({ 
+         res.status(200).json({ 
 //             success: true, 
 //             message: "Account created successfully! ", 
 //             data: result 
