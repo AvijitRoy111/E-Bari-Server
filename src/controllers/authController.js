b6bf534c2b045d94fdd24c64c1b1f98abdd806d6
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-20 16:20:05 +0600

    added the match password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -62,7 +62,7 @@ const createUser = async (req, res) => {
 
           // ২. check password match
           if (user.password !== password) {
-//              return res.status(401).json({ 
+              return res.status(401).json({ 
 //                  success: false, 
 //                  message: "Invalid password! Please try again." 
 //              });
