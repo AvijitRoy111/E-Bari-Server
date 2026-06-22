66fc96fafd29c88aa428f5b4124dd9dd09a809ce
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-22 22:35:05 +0600

    added the api

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -9,7 +9,7 @@
 
  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
  const client = new MongoClient(uri, {
-//   serverApi: {
+   serverApi: {
 //     version: ServerApiVersion.v1,
 //     strict: true,
 //     deprecationErrors: true,
