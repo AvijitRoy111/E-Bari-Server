139acec0a8859a8df627d36ef778124771453b83
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-22 22:30:31 +0600

    added the catch

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -78,7 +78,7 @@ const createUser = async (req, res) => {
 
       } catch (error) {
           res.status(500).json({ success: false, message: "Internal Server Error" });
-//      }
+      }
 //  }
 
 //  module.exports = {
