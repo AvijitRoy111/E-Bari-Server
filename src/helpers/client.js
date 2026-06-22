fa2bd617e35c813fbe6a264aa27dacbe6664e015
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-22 22:34:56 +0600

    added the api

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -8,7 +8,7 @@
  const uri = "mongodb+srv://Bari-Master:epwAKdwF6t5D6fXg@cluster0.tkbsmtm.mongodb.net/?appName=Cluster0";
 
  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
+ const client = new MongoClient(uri, {
 //   serverApi: {
 //     version: ServerApiVersion.v1,
 //     strict: true,
