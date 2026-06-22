ab8b545254c47b2f1dca5234d10a75303096a7b8
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-22 22:33:37 +0600

    added the exports

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,22 +1,22 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
-//BariMaster FZC4hqWOwSid0DCs I0YydlFAN8l8eCLP Bari-Master  epwAKdwF6t5D6fXg
+// //BariMaster FZC4hqWOwSid0DCs I0YydlFAN8l8eCLP Bari-Master  epwAKdwF6t5D6fXg
 
 
 
-const uri = "mongodb+srv://Bari-Master:epwAKdwF6t5D6fXg@cluster0.tkbsmtm.mongodb.net/?appName=Cluster0";
+// const uri = "mongodb+srv://Bari-Master:epwAKdwF6t5D6fXg@cluster0.tkbsmtm.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-module.exports = client ;
+// module.exports = client ;
 
 
 
