e663ea56e263fa11eb0d2bb85df6881d49f3deba
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-23 20:57:14 +0600

    added the routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const {
-    createUser,
-    getCurrentUser,
-    loginUser
-} = require('../controllers/authController')
+// const express = require('express');
+// const {
+//     createUser,
+//     getCurrentUser,
+//     loginUser
+// } = require('../controllers/authController')
 
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/create-user', createUser);
-router.get('/current-user', getCurrentUser);
-router.post('/login', loginUser);
+// router.post('/create-user', createUser);
+// router.get('/current-user', getCurrentUser);
+// router.post('/login', loginUser);
 
-module.exports = router;
+// module.exports = router;
