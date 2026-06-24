311adf943fab6c123f33ff1973c184ef278b2a95
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-06-24 17:05:05 +0600

    added the require

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
     createContact
 
-// } = require('../controllers/contactController');
+} = require('../controllers/contactController');
 
 // const router = express.Router();
 
